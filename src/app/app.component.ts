f5596ae8ef1208006d708ed42d93eca7911243be
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2018-01-25 08:06:45 +0000

    fixed theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,155 +1,159 @@
-import {Component, ElementRef, ViewChild, AfterViewInit, OnDestroy} from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 declare var jQuery: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit, OnDestroy  {
+export class AppComponent implements AfterViewInit, OnDestroy {
 
-  themeSwitch = false;
+    darkTheme = false;
 
-  public menuMode = 'static';
+    menuMode = 'static';
 
-  topbarMenuActive: boolean;
+    topbarMenuActive: boolean;
 
-  overlayMenuActive: boolean;
+    overlayMenuActive: boolean;
 
-  staticMenuDesktopInactive: boolean;
+    staticMenuDesktopInactive: boolean;
 
-  staticMenuMobileActive: boolean;
+    staticMenuMobileActive: boolean;
 
-  layoutMenuScroller: HTMLDivElement;
+    layoutMenuScroller: HTMLDivElement;
 
-  menuClick: boolean;
+    menuClick: boolean;
 
-  topbarItemClick: boolean;
+    topbarItemClick: boolean;
 
-  activeTopbarItem: any;
+    activeTopbarItem: any;
 
-  resetMenu: boolean;
+    resetMenu: boolean;
 
-  menuHoverActive: boolean;
+    menuHoverActive: boolean;
 
-  @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ElementRef;
+    @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ElementRef;
 
-  ngAfterViewInit() {
-    this.layoutMenuScroller = <HTMLDivElement> this.layoutMenuScrollerViewChild.nativeElement;
+    ngAfterViewInit() {
+        this.layoutMenuScroller = <HTMLDivElement>this.layoutMenuScrollerViewChild.nativeElement;
 
-    setTimeout(() => {
-      jQuery(this.layoutMenuScroller).nanoScroller({flash: true});
-    }, 10);
-  }
-
-  onLayoutClick() {
-    if (!this.topbarItemClick) {
-      this.activeTopbarItem = null;
-      this.topbarMenuActive = false;
+        setTimeout(() => {
+            jQuery(this.layoutMenuScroller).nanoScroller({ flash: true });
+        }, 10);
     }
 
-    if (!this.menuClick) {
-      if (this.isHorizontal() || this.isSlim()) {
-        this.resetMenu = true;
-      }
+    onLayoutClick() {
+        if (!this.topbarItemClick) {
+            this.activeTopbarItem = null;
+            this.topbarMenuActive = false;
+        }
 
-      if (this.overlayMenuActive || this.staticMenuMobileActive) {
+        if (!this.menuClick) {
+            if (this.isHorizontal() || this.isSlim()) {
+                this.resetMenu = true;
+            }
+
+            if (this.overlayMenuActive || this.staticMenuMobileActive) {
+                this.hideOverlayMenu();
+            }
+
+            this.menuHoverActive = false;
+        }
+
+        this.topbarItemClick = false;
+        this.menuClick = false;
+    }
+
+    onMenuButtonClick(event) {
+        this.menuClick = true;
+        this.topbarMenuActive = false;
+
+        if (this.isOverlay()) {
+            this.overlayMenuActive = !this.overlayMenuActive;
+        }
+        if (this.isDesktop()) {
+            this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
+        } else {
+            this.staticMenuMobileActive = !this.staticMenuMobileActive;
+        }
+
+        event.preventDefault();
+    }
+
+    onMenuClick($event) {
+        this.menuClick = true;
+        this.resetMenu = false;
+
+        if (!this.isHorizontal()) {
+            setTimeout(() => {
+                jQuery(this.layoutMenuScroller).nanoScroller();
+            }, 500);
+        }
+    }
+
+    onTopbarMenuButtonClick(event) {
+        this.topbarItemClick = true;
+        this.topbarMenuActive = !this.topbarMenuActive;
+
         this.hideOverlayMenu();
-      }
 
-      this.menuHoverActive = false;
+        event.preventDefault();
     }
 
-    this.topbarItemClick = false;
-    this.menuClick = false;
-  }
-  onMenuButtonClick(event) {
-    this.menuClick = true;
-    this.topbarMenuActive = false;
+    onTopbarItemClick(event, item) {
+        this.topbarItemClick = true;
 
-    if (this.isOverlay()) {
-      this.overlayMenuActive = !this.overlayMenuActive;
+        if (this.activeTopbarItem === item) {
+            this.activeTopbarItem = null;
+        } else {
+            this.activeTopbarItem = item;
+        }
+
+        event.preventDefault();
     }
-    if (this.isDesktop()) {
-      this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
-    } else {
-      this.staticMenuMobileActive = !this.staticMenuMobileActive; }
 
-    event.preventDefault();
-  }
-
-  onMenuClick($event) {
-    this.menuClick = true;
-    this.resetMenu = false;
-
-    if (!this.isHorizontal()) {
-      setTimeout(() => {
-        jQuery(this.layoutMenuScroller).nanoScroller();
-      }, 500);
+    isHorizontal() {
+        return this.menuMode === 'horizontal';
     }
-  }
 
-  onTopbarMenuButtonClick(event) {
-    this.topbarItemClick = true;
-    this.topbarMenuActive = !this.topbarMenuActive;
-
-    this.hideOverlayMenu();
-
-    event.preventDefault();
-  }
-
-  onTopbarItemClick(event, item) {
-    this.topbarItemClick = true;
-
-    if (this.activeTopbarItem === item) {
-      this.activeTopbarItem = null; } else {
-      this.activeTopbarItem = item; }
-
-    event.preventDefault();
-  }
-
-  isHorizontal() {
-    return this.menuMode === 'horizontal';
-  }
-
-  isSlim() {
-    return this.menuMode === 'slim';
-  }
-
-  isOverlay() {
-    return this.menuMode === 'overlay';
-  }
-
-  isStatic() {
-    return this.menuMode === 'static';
-  }
-
-  isMobile() {
-    return window.innerWidth < 1025;
-  }
-
-  isDesktop() {
-    return window.innerWidth > 1024;
-  }
-
-  hideOverlayMenu() {
-    this.overlayMenuActive = false;
-    this.staticMenuMobileActive = false;
-  }
-
-  changeTheme(theme) {
-    const themeLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('theme-css');
-    themeLink.href = 'assets/theme/theme-' + theme + '.css';
-    const layoutLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('layout-css');
-    layoutLink.href = 'assets/layout/css/layout-' + theme + '.css';
-    if (theme.includes('dark')) {
-      this.themeSwitch = true;
-    } else {
-      this.themeSwitch = false;
+    isSlim() {
+        return this.menuMode === 'slim';
     }
-  }
 
-  ngOnDestroy() {
-    jQuery(this.layoutMenuScroller).nanoScroller({flash: true});
-  }
+    isOverlay() {
+        return this.menuMode === 'overlay';
+    }
+
+    isStatic() {
+        return this.menuMode === 'static';
+    }
+
+    isMobile() {
+        return window.innerWidth < 1025;
+    }
+
+    isDesktop() {
+        return window.innerWidth > 1024;
+    }
+
+    hideOverlayMenu() {
+        this.overlayMenuActive = false;
+        this.staticMenuMobileActive = false;
+    }
+
+    changeTheme(theme) {
+        const themeLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-css');
+        themeLink.href = 'assets/theme/theme-' + theme + '.css';
+        const layoutLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('layout-css');
+        layoutLink.href = 'assets/layout/css/layout-' + theme + '.css';
+        
+        if (theme.indexOf('dark') !== -1)
+            this.darkTheme = true;
+        else
+            this.darkTheme = false;
+    }
+
+    ngOnDestroy() {
+        jQuery(this.layoutMenuScroller).nanoScroller({ flash: true });
+    }
 }
--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {AppComponent} from './app.component';
+import { Component } from '@angular/core';
+import { AppComponent } from './app.component';
 
 @Component({
     selector: 'app-topbar',
@@ -7,21 +7,17 @@ import {AppComponent} from './app.component';
 })
 export class AppTopBarComponent {
 
-    constructor(public app: AppComponent) {}
+    constructor(public app: AppComponent) { }
 
     themeChange(e) {
-        let theme, themeStyle;
         const themeLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-css');
-        for (const element of themeLink.href.split('/')) {
-          if (element.includes('-')) {
-            theme = element.split('-')[1];
-            themeStyle = element.split('-')[2].split('.')[0];
-          }
-        }
-        if (themeStyle === 'dark') {
-          this.app.changeTheme(theme + '-' + 'light');
-        } else {
-          this.app.changeTheme(theme + '-' + 'dark');
-        }
+        let href = themeLink.href;
+        let themeFile = href.substring(href.lastIndexOf('/') + 1, href.lastIndexOf('.'));
+        let themeTokens = themeFile.split('-');
+        let themeName = themeTokens[1];
+        let themeMode = themeTokens[2];
+        let newThemeMode = (themeMode === 'dark') ? 'light': 'dark';
+
+        this.app.changeTheme(themeName + '-' + newThemeMode);
     }
 }
