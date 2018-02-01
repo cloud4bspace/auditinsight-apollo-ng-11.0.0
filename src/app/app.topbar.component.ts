54c6dfad3ab512f84db3e0c0d3280cbb9c005589
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-02-01 11:26:33 +0000

    splitbutton fix

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -11,12 +11,12 @@ export class AppTopBarComponent {
 
     themeChange(e) {
         const themeLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-css');
-        let href = themeLink.href;
-        let themeFile = href.substring(href.lastIndexOf('/') + 1, href.lastIndexOf('.'));
-        let themeTokens = themeFile.split('-');
-        let themeName = themeTokens[1];
-        let themeMode = themeTokens[2];
-        let newThemeMode = (themeMode === 'dark') ? 'light': 'dark';
+        const href = themeLink.href;
+        const themeFile = href.substring(href.lastIndexOf('/') + 1, href.lastIndexOf('.'));
+        const themeTokens = themeFile.split('-');
+        const themeName = themeTokens[1];
+        const themeMode = themeTokens[2];
+        const newThemeMode = (themeMode === 'dark') ? 'light' : 'dark';
 
         this.app.changeTheme(themeName + '-' + newThemeMode);
     }
