f924acc3774d676b398b520204bdf7b830cf2f7a
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-06-20 08:29:02 +0000

    dataview and treetable support

--- src/app/demo/view/datademo.component.ts
+++ src/app/demo/view/datademo.component.ts
@@ -3,20 +3,28 @@ import { CarService } from '../service/carservice';
 import { NodeService } from '../service/nodeservice';
 import { EventService } from '../service/eventservice';
 import { Car } from '../domain/car';
-import { TreeNode } from 'primeng/primeng';
+import { TreeNode, SelectItem } from 'primeng/primeng';
 import { BreadcrumbService } from '../../breadcrumb.service';
 
 @Component({
     templateUrl: './datademo.component.html',
     styles: [`
-        .cars-datalist ul {
-            margin: 0;
-            padding: 0;
+        .ui-dataview .filter-container {
+            text-align: center;
         }
 
-        @media (max-width:640px) {
-            .cars-datalist .text-column {
+        @media (max-width: 40em) {
+            .ui-dataview .car-details, .ui-dataview .search-icon{
                 text-align: center;
+                margin-top: 0;
+            }
+
+            .ui-dataview .filter-container {
+                text-align: left;
+            }
+            .ui-dataview-layout-options.ui-buttonset > .ui-button {
+                margin-left: 0;
+                display: inline-block;
             }
         }
     `],
@@ -28,13 +36,13 @@ export class DataDemoComponent implements OnInit {
 
     cars2: Car[];
 
-    cars3: Car[];
-
     cols: any[];
+
+    cols2: any[];
 
     data: TreeNode[];
 
-    selectedNode: TreeNode;
+    selectedNodeOrg: TreeNode;
 
     selectedCar: Car;
 
@@ -50,19 +58,35 @@ export class DataDemoComponent implements OnInit {
 
     files2: TreeNode[];
 
+    files3: TreeNode[];
+
+    files4: TreeNode[];
+
     events: any[];
 
     selectedNode1: TreeNode;
 
     selectedNode2: TreeNode;
 
+    selectedNode3: TreeNode;
+
+    selectedNodes: TreeNode[];
+
     scheduleHeader: any;
 
+    sortOptions: SelectItem[];
+
+    sortKey: string;
+
+    sortField: string;
+
+    sortOrder: number;
+
     constructor(private carService: CarService, private eventService: EventService, private nodeService: NodeService,
-        private breadcrumbService: BreadcrumbService) {
+                private breadcrumbService: BreadcrumbService) {
         this.breadcrumbService.setItems([
             { label: 'Components' },
-            { label: 'Data', routerLink: ['/data'] }
+            { label: 'Data Components', routerLink: ['/data'] }
         ]);
     }
 
@@ -74,28 +98,34 @@ export class DataDemoComponent implements OnInit {
             { field: 'brand', header: 'Brand' },
             { field: 'color', header: 'Color' }
         ];
+        this.cols2 = [
+            { field: 'name', header: 'Name' },
+            { field: 'size', header: 'Size' },
+            { field: 'type', header: 'Type' }
+        ];
         this.carService.getCarsMedium().then(cars => this.cars2 = cars);
-        this.carService.getCarsMedium().then(cars => this.cars3 = cars);
         this.carService.getCarsMedium().then(cars => this.sourceCars = cars);
         this.targetCars = [];
         this.carService.getCarsSmall().then(cars => this.orderListCars = cars);
         this.nodeService.getFiles().then(files => this.files1 = files);
-        this.nodeService.getFilesystem().then(files => this.files2 = files);
-        this.eventService.getEvents().then(events => { this.events = events; });
+        this.nodeService.getFiles().then(files => this.files2 = files);
+        this.nodeService.getFiles().then(files => this.files3 = files);
+        this.nodeService.getFilesystem().then(files => this.files4 = files);
+        this.eventService.getEvents().then(events => {this.events = events; });
 
         this.carouselCars = [
-            { vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black' },
-            { vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White' },
-            { vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue' },
-            { vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White' },
-            { vin: 'gf45wg5', year: 2011, brand: 'Volkswagen', color: 'Red' },
-            { vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue' },
-            { vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow' },
-            { vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown' },
-            { vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black' }
+            {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
+            {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+            {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
+            {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
+            {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
+            {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
+            {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
+            {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
+            {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
         ];
 
-        this.scheduleHeader = { left: 'prev,next today', center: 'title', right: 'month,agendaWeek,agendaDay' };
+        this.scheduleHeader = { left: 'prev,next today', center: 'title', right: 'month,agendaWeek,agendaDay'};
 
         this.data = [{
             label: 'F.C Barcelona',
@@ -127,5 +157,23 @@ export class DataDemoComponent implements OnInit {
                 }
             ]
         }];
+
+        this.sortOptions = [
+            { label: 'Newest First', value: '!year' },
+            { label: 'Oldest First', value: 'year' },
+            { label: 'Brand', value: 'brand' }
+        ];
+    }
+
+    onSortChange(event) {
+        const value = event.value;
+
+        if (value.indexOf('!') === 0) {
+            this.sortOrder = -1;
+            this.sortField = value.substring(1, value.length);
+        } else {
+            this.sortOrder = 1;
+            this.sortField = value;
+        }
     }
 }
